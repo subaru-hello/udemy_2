1a85582ad3169b55a1f24a0a78c7f3f36e6b388d
subaru-hello <octpsubaru@icloud.com>
2021-06-09 19:45:36 +0900

    Bootstrap 追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+/
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= rewuire bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
