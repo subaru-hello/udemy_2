2a2834a73e898690b01c6f74239a0e085278af40
subaru-hello <octpsubaru@icloud.com>
2021-06-09 23:00:39 +0900

    6月9日最後のコミット

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,4 +3,16 @@ class BoardsController < ApplicationController
   def index
   
   end
+
+  def new
+    @board = Board.new 
+  end
+  def create
+    Board.create(board_params)
+  end
+
+private
+def board_params
+  params.require(:board).permit(:name, :title, :body)
+end
 end
