e5c3c138e3a96d3f7b80fda37a4970c48bdc1abb
subaru-hello <octpsubaru@icloud.com>
2021-06-10 07:16:27 +0900

    掲示板新規作成ページの作成 #2

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,11 @@
 
 class BoardsController < ApplicationController
   def index
-  
+    @boards = Board.all 
   end
 
   def new
-    @board = Board.new 
+    @boards = Board.new 
   end
   def create
     Board.create(board_params)
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:datetime_jp] = '%Y年 %M月 %D日 %H% %M分'
