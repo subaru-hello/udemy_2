42f77b37a53b6624cfc43e10af1a62cbc5066e96
subaru-hello <octpsubaru@icloud.com>
2021-06-11 21:49:00 +0900

    1対多の関係

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ApplicationRecord
+  has_many :comments
 validates :name, presence: true, length: {maximum: 10}
 validates :title, presence: true, length: {maximum: 30}
 validates :body, presence: true, length: {maximum: 1000}
